2a8054c2fbb0204daa232697ad2a2a325298efbc
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-11-20 15:59:12 +0400

    location of the state on the map

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -6,6 +7,24 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+
+
+
+
 answer_state = screen.textinput(title = "Guess the State", prompt="What is another state is name?")
 print(answer_state)
+
+if answer_state in all_states:
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    state_data = data[data.state == answer_state]
+    t.goto(int(state_data.x),int(state_data.y))
+    t.write(state_data.state)
+
+screen.exitonclick()
+
 
