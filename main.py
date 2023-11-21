38e8053b7dd15ff344e49db7815a9c9c51249480
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-11-21 13:27:40 +0400

    Writing the code in list form by reducing it

--- main.py
+++ main.py
@@ -18,9 +18,7 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title = "Guess the State", prompt="What is another state is name?").title()
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.c_to_learn.csv")
         break
